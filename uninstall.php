04196610d39a6d37bb2c82f7267ed0d2f8f194ad
Marta Torre <hola@martatorre.dev>
2024-11-11 17:02:01 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,16 @@
+<?php
+
+// If uninstall.php is not called by WordPress, exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+
+// Delete the plugin option from the database
+delete_option( 'human_tagger_humans_text' );
+
+// Delete the humans.txt file from the root directory
+$file_path = ABSPATH . 'humans.txt';
+if ( file_exists( $file_path ) ) {
+    unlink( $file_path );
+}
 
